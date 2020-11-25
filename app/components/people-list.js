ac4b565cf89138febb3787733d4e252628d8b176
cengizcmataraci <cengizcmataraci@gmail.com>
2020-11-25 16:34:50 +0300

    Ember.js Quick Start

---
+++ app/components/people-list.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class PeopleListComponent extends Component {
+  @action
+  showPerson(person) {
+    alert(`The person's name is ${person}!`);
+  }
+
+  send(sen) {
+    alert(`Post is sended.`);
+  }
+}
